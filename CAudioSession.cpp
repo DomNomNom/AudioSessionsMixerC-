4a4c303bd423d8f3f9fe67fc2842e6e886859cb7
Dominik Schmid <dominikschmid93@gmail.com>
2021-04-30 19:22:03 +1000

    not delete session controls

--- CAudioSession.cpp
+++ CAudioSession.cpp
@@ -11,11 +11,13 @@ CAudioSession::CAudioSession()
 
 CAudioSession::~CAudioSession()
 {
-	/*SAFE_RELEASE(pSessionControl);
-	SAFE_RELEASE(pSessionControl2);*/
 	int hr;
 	CHECK_HR(hr = pSessionControl->UnregisterAudioSessionNotification(eventListener.get()));
+	/*SAFE_RELEASE(pSessionControl);
+	SAFE_RELEASE(pSessionControl2);*/
+	/*
 	SAFE_DELETE(pSessionControl);
 	SAFE_DELETE(pSessionControl2);
 	SAFE_DELETE(pSessionVolumeCtrl);
+	*/
 }
